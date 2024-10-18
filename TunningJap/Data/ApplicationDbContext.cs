f540de8fd611926b9b15998a7bf3d94eeed8fadd
eon <dobrin_k1@abv.bg>
2024-10-18 12:53:12 +0300

    Add project files.

---
+++ TunningJap/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TunningJap.Models;
+
+namespace TunningJap.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TunningJap.Models.Car> Car { get; set; } = default!;
+    }
+}
---
+++ TunningJap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TunningJap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
